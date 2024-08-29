1b77ab8a34a66f0a76b6fa08885abfeb52cbc130
Kyle Lanclos <klanclos@keck.hawaii.edu>
2024-08-28 17:00:46 -1000

    Top-level module structure.

---
+++ python/__init__.py
@@ -0,0 +1,11 @@
+''' Python implementation of POT. This includes client functions, such as
+    interacting with key/value stores, and daemon functions, such as publishing
+    key/value pairs and handling client requests.
+'''
+
+from . import Get
+from . import Store
+
+get = Get.get
+
+# vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
