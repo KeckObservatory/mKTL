7ab590dba6cad8d4718a0b5ad99d59f5d985efcf
Kyle Lanclos <klanclos@keck.hawaii.edu>
2024-09-12 14:53:03 -1000

    vim helpers.

--- python/lib/Protocol/__init__.py
+++ python/lib/Protocol/__init__.py
@@ -2,3 +2,5 @@
 from . import Discover
 from . import Publish
 from . import Request
+
+# vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
--- python/lib/Proxy/__init__.py
+++ python/lib/Proxy/__init__.py
@@ -2,3 +2,5 @@ from . import Common
 from . import KTL
 
 KTL = KTL.KTL
+
+# vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
