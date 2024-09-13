d20410c101470b5e804f026d25855cf0d92edcc4
Kyle Lanclos <klanclos@keck.hawaii.edu>
2024-09-12 14:14:58 -1000

    Moving components around.

---
+++ python/lib/__init__.py
@@ -0,0 +1,14 @@
+''' Python implementation of POT. This includes client functions, such as
+    interacting with key/value stores, and daemon functions, such as publishing
+    key/value pairs and handling client requests.
+'''
+
+from . import WeakRef
+
+from . import Protocol
+from . import Proxy
+
+from . import Get
+get = Get.get
+
+# vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
