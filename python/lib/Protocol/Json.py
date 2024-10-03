4442877dd926bce2c90e57c0f699961fbaeeed1f
Kyle Lanclos <klanclos@keck.hawaii.edu>
2024-10-02 16:55:55 -1000

    Starting to cook up a Json wrapper.

---
+++ python/lib/Protocol/Json.py
@@ -0,0 +1,38 @@
+''' Wrapper module to select the most performant available library to handle
+    the equivalent of :func:`json.loads` and :func:`json.dumps`.
+'''
+
+# The business about conditionally importing the libraries is intended to
+# avoid importing less efficient libraries if they are not available. With
+# the right build process this could be determined at build time, instead
+# of at run time.
+
+try:
+    import msgspec
+except ImportError:
+    msgspec = None
+else:
+    orjson = None
+    json = None
+
+if msgspec is None:
+    try:
+        import orjson
+    except ImportError:
+        orjson = None
+    else:
+        json = None
+
+if msgspec is None and orjson is None:
+    import json
+
+
+# msgspec returns bytes.
+orjson_dumps
+
+if msgspec is not None:
+    dumps = msgspec.json.dumps
+    loads = msgspec.json.loads
+elif orjson is not None:
+
+# vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
