9c153103745b74f1b12c0d3f59a4a1465e82eb8f
Kyle Lanclos <klanclos@keck.hawaii.edu>
2025-01-06 11:41:50 -1000

    Add a method to append a provenance to a configuration block.

--- python/lib/Config.py
+++ python/lib/Config.py
@@ -20,4 +20,31 @@ def get(name):
     return
 
 
+
+def addProvenance(block, hostname, port):
+    ''' Add the provenance of this daemon to the supplied configuration
+        block. The block is provided as a Python dictionary; the hostname
+        and port combine to provide a unique location identifier that clients
+        can use to track the chain of handling for this configuration data.
+    '''
+
+    try:
+        full_provenance = block['provenance']
+    except KeyError:
+        full_provenance = list()
+        block['provenance'] = full_provenance
+
+    stratum = -1
+    for provenance in full_provenance:
+        if provenance['stratum'] > stratum:
+            stratum = provenance['stratum']
+
+    provenance = dict()
+    provenance['stratum'] = stratum + 1
+    provenance['hostname'] = hostname
+    provenance['port'] = port
+
+    block['provenance'].append(provenance)
+
+
 # vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
