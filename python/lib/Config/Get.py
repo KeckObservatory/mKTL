037434d22d2d051ed733b9353e4dcb78bd14038d
Kyle Lanclos <klanclos@keck.hawaii.edu>
2025-01-08 13:35:30 -1000

    Renamed Config.py to be part of a Config submodule.

---
+++ python/lib/Config/Get.py
@@ -0,0 +1,23 @@
+
+from ..Protocol import Discover
+from ..Protocol import Request
+
+
+def get(name):
+    ''' Retrieve the mKTL configuration for the specified `name`.
+    '''
+
+    ## check the cache
+
+    ## confirm the current version is up-to-date by contacting the last known
+    ## source of authority and asking for the current hash
+
+    ## if the cache is not available, or the cache is stale, retrieve the
+    ## current configuration from the last known source of authority
+
+    ## return the configuration
+
+    return
+
+
+# vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
