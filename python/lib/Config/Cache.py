b5efc409a64cb569c66fdeba79f0243530322227
Kyle Lanclos <klanclos@keck.hawaii.edu>
2025-01-10 15:32:16 -1000

    Use the adjacent Hash components.

--- python/lib/Config/Cache.py
+++ python/lib/Config/Cache.py
@@ -1,8 +1,9 @@
 ''' Local storage for configuration data.
 '''
 
-cache = dict()
+from . import Hash
 
+cache = dict()
 
 def add(name, data):
     ''' Add a configuration block to the local cache.
@@ -11,13 +12,23 @@ def add(name, data):
     try:
         blocks = cache[name]
     except KeyError:
-        blocks = []             # list() is now the method defined here.
+        blocks = []             # list() was redefined locally
         cache[name] = blocks
+
+    try:
+        data['hash']
+    except KeyError:
+        data['hash'] = Hash.hash(data['keys'])
 
     ## Does there need to be a provenance check here?
     ## What about duplicate keys?
 
+    def get_hash(config_block):
+        return config_block['hash']
+
     blocks.append(data)
+    blocks.sort(key=get_hash)
+    Hash.rehash(name)
 
 
 def get(name):
@@ -28,7 +39,7 @@ def get(name):
     try:
         blocks = cache[name]
     except KeyError:
-        blocks = []             # list() is now the method defined here.
+        blocks = []             # list() was redefined locally
         cache[name] = blocks
 
     if len(blocks) == 0:
@@ -42,7 +53,7 @@ def list():
     '''
 
     names = cache.keys()
-    results = []                # list() is now the method defined here.
+    results = []                # list() was redefined locally
 
     for name in names:
         blocks = cache[name]
@@ -62,6 +73,7 @@ def remove(name, data):
         raise KeyError('no local configuration for ' + repr(name))
 
     blocks.remove(data)
+    Hash.rehash(name)
 
 
 # vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
