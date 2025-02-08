dbb75067f9d7eb0ef1603fb1d14cc29f1529abf5
Kyle Lanclos <klanclos@keck.hawaii.edu>
2025-02-07 15:16:19 -1000

    Establish top-level functions so other code doesn't have to know which Config submodule file to reach into.

--- python/lib/Config/__init__.py
+++ python/lib/Config/__init__.py
@@ -1,9 +1,12 @@
 
 from . import Cache
+from . import File
 from . import Hash
 from . import Items
 from . import Provenance
 
-from . import Get
+add = Cache.add
+get = Cache.get
+load = File.load
 
 # vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
