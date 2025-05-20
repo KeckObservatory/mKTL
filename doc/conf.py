48d11c9b5ea92a1302f010c4cece0757c47e1726
Kyle Lanclos <klanclos@keck.hawaii.edu>
2025-05-19 17:08:35 -1000

    Tweaks to enable autodoc.

--- doc/conf.py
+++ doc/conf.py
@@ -15,7 +15,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -28,3 +28,6 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 html_css_files = ['css/custom.css']
+
+import sys
+sys.path.append('.')
