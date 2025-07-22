0e7f1b261e60b673ba8d3245c128726ad8e8030e
Kyle Lanclos <klanclos@keck.hawaii.edu>
2025-07-21 16:21:30 -1000

    Daemon components moving around.

--- src/mktl/__init__.py
+++ src/mktl/__init__.py
@@ -7,15 +7,24 @@ from . import WeakRef
 
 from . import Protocol
 from . import Config
-from . import Daemon
 from . import BackEnd
 
 from . import Get
 get = Get.get
 home = Config.File.home
 
+# These next pieces are used internally by daemon.py, though the polling
+# component is also used in item.py.
+
+from . import persist
+from . import poll
+from . import port
+
+# These are the main public-facing classes.
+
 from .item import Item
 from .store import Store
+from .daemon import Daemon
 
 
 # vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
