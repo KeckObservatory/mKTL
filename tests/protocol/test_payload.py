fc27d631678124b718ec7275490c4532f5d6a273
Kyle Lanclos <klanclos@keck.hawaii.edu>
2026-02-12 16:00:38 -1000

    More payload testing.

--- tests/protocol/test_payload.py
+++ tests/protocol/test_payload.py
@@ -1,4 +1,5 @@
 import mktl
+import pytest
 import time
 
 def test_basics():
@@ -14,16 +15,68 @@ def test_basics():
         assert payload.error == None
         assert payload.refresh == None
         assert payload.shape == None
+        ## Pull request is pending
         ##assert payload.reply == True
+        payload.encapsulate()
 
 
+def test_encapsulate():
 
-##def test_encapsulate():
+    test_value = 44
+    for test_value in (44, True, None, 35.5, [1,2,3], 'string'):
+        payload = mktl.protocol.message.Payload(test_value)
 
-##def test_kwargs():
+        encapsulated = payload.encapsulate()
+        assert isinstance(encapsulated, bytes)
 
-##def test_omission():
+        decoded = mktl.json.loads(encapsulated)
+        assert isinstance(decoded, dict)
 
-##def test_origin()
+        assert 'time' in decoded
+        assert 'value' in decoded
+        assert decoded['value'] == test_value
+
+
+    bad_payload = mktl.protocol.message.Payload({None: 'none'})
+
+    with pytest.raises(TypeError):
+        bad_payload.encapsulate()
+
+
+def test_kwargs():
+
+    payload = mktl.protocol.message.Payload('something', testing='testing')
+    assert payload.testing == 'testing'
+
+    encapsulated = payload.encapsulate()
+    decoded = mktl.json.loads(encapsulated)
+
+    assert 'time' in decoded
+    assert 'value' in decoded
+    assert 'testing' in decoded
+
+    payload.omit.add('testing')
+
+    encapsulated = payload.encapsulate()
+    decoded = mktl.json.loads(encapsulated)
+
+    assert not 'testing' in decoded
+
+
+def test_origin():
+
+    payload = mktl.protocol.message.Payload('something')
+    payload.add_origin()
+
+    encapsulated = payload.encapsulate()
+    decoded = mktl.json.loads(encapsulated)
+
+    assert '_user' in decoded
+    assert '_hostname' in decoded
+    assert '_pid' in decoded
+    assert '_ppid' in decoded
+    assert '_executable' in decoded
+    assert '_argv' in decoded
+
 
 # vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
