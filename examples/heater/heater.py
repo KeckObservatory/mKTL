b148f5fb2f76da829be8ec89f9ab10363840860c
Kyle Lanclos <klanclos@keck.hawaii.edu>
2026-08-04 12:00:33 -1000

    Generating the item descriptions.

--- examples/heater/heater.py
+++ examples/heater/heater.py
@@ -1,7 +1,114 @@
+""" This daemon connects to a fictional temperature controller. It is
+    structured to relay commands and publish available telemetry from
+    the controller.
+
+    As a fictional example, this code has not been directly tested, and may
+    contain syntax errors, logical errors, and other miscellaneous problems.
+"""
+
+import configparser
 import mktl
 
 
 class Daemon(mktl.Daemon):
+
+    def parse_options(self, options):
+
+        configuration_file = options.appconfig
+
+        parser = configparser.ConfigParser()
+        parser.read(configuration_file)
+
+        self.heater_config = parser
+
+
+    def describe_items(self):
+
+        items = dict()
+
+        controller_number = self.heater_config.get('main', 'controller')
+        controller_items = self.describe_controller_items(controller_number)
+
+        items.update(controller_items)
+
+
+        try:
+            inputs = self.heater_config.options('inputs')
+        except configparser.NoSectionError:
+            inputs = tuple()
+
+        try:
+            outputs = self.heater_config.options('outputs')
+        except configparser.NoSectionError:
+            outputs = tuple()
+
+        for input in inputs:
+            prefix = main.config.get('inputs', input)
+
+            chi = prefix + 'chi'
+            items[chi] = dict()
+            items[chi]['description'] = 'Channel for this temperature input.'
+            items[chi]['settable'] = False
+
+            tmp = prefix + 'tmp'
+            items[tmp] = dict()
+            items[tmp]['description'] = 'Current temperature value.'
+            items[tmp]['type'] = 'numeric'
+            items[tmp]['units'] = 'deg C'
+            items[tmp]['settable'] = False
+
+        for output in outputs:
+            cho = prefix + 'cho'
+            items[cho] = dict()
+            items[cho]['description'] = 'Channel for this heater output.'
+            items[cho]['settable'] = False
+
+            out = prefix + 'out'
+            items[out] = dict()
+            items[out]['description'] = 'Current heater output.'
+            items[out]['type'] = 'numeric'
+            items[out]['units'] = 'watts'
+            items[out]['settable'] = False
+
+            trg = prefix + 'trg'
+            items[trg] = dict()
+            items[trg]['description'] = 'Heater setpoint/target value.'
+            items[trg]['type'] = 'numeric'
+            items[trg]['units'] = 'deg C'
+
+
+        return items
+
+
+    def describe_controller_items(self, controller):
+
+        controller = str(controller)
+        prefix = 'ctrl' + controller
+
+        items = dict()
+
+        address = prefix + 'address'
+        items[address] = dict()
+        items[address]['description'] = 'Controller IP address.'
+        items[address]['settable'] = False
+
+        aux = prefix + 'aux'
+        items[aux] = dict()
+        items[aux]['description'] = 'Auxiliary command and response.'
+
+        firmware = prefix + 'firmware'
+        items[firmware] = dict()
+        items[firmware]['description'] = 'Controller firmware revision.'
+        items[firmware]['settable'] = False
+
+        status = prefix + 'status'
+        items[status] = dict()
+        items[status]['description'] = 'Controller connection status.'
+        items[status]['type'] = 'enumerated'
+        items[status]['settable'] = False
+
+        return items
+
 
     def setup(self):
 
