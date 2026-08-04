686416b6e8514765e82971f987c28eb927e2c499
Kyle Lanclos <klanclos@keck.hawaii.edu>
2026-08-04 09:15:11 -1000

    Use the Daemon.describe_items() hook.

--- tests/unitdaemon.py
+++ tests/unitdaemon.py
@@ -4,79 +4,71 @@
 """
 
 import mktl
-import time
 
 
 class Daemon(mktl.Daemon):
 
-    def __init__(self, store, alias, *args, **kwargs):
+    def describe_items(self):
 
-        items = generate_catalog()
-        mktl.meta.authoritative(store, alias, items)
-        mktl.Daemon.__init__(self, store, alias, *args, **kwargs)
+        items = dict()
+
+        items['angle'] = dict()
+        items['angle']['description'] = 'An angular numeric item.'
+        items['angle']['type'] = 'numeric'
+        items['angle']['format'] = '%2d:%2.2d:%04.1f'
+        items['angle']['units'] = {'': 'radians', 'formatted': 'degrees'}
+        items['angle']['initial'] = 0.018049613347708025
+
+        items['boolean'] = dict()
+        items['boolean']['description'] = 'A boolean item without enumerators.'
+        items['boolean']['type'] = 'boolean'
+
+        items['enumerated'] = dict()
+        items['enumerated']['description'] = 'An enumerated item.'
+        items['enumerated']['type'] = 'enumerated'
+        items['enumerated']['enumerators'] = {0: 'Zero', 1: 'One', 4: 'Four'}
+
+        items['hourangle'] = dict()
+        items['hourangle']['description'] = 'An angular numeric item, in h:m:s.'
+        items['hourangle']['type'] = 'numeric'
+        items['hourangle']['format'] = '%2d:%2.2d:%04.1f'
+        items['hourangle']['units'] = {'': 'radians', 'formatted': 'hours'}
+        items['hourangle']['initial'] = 0.2707442002156204
+
+        items['mask'] = dict()
+        items['mask']['description'] = 'A mask item.'
+        items['mask']['type'] = 'mask'
+        items['mask']['enumerators'] = {'None': 'none set', 0: 'A', 1: 'B', 2: 'C'}
+
+        items['noyes'] = dict()
+        items['noyes']['description'] = 'A boolean item with enumerators.'
+        items['noyes']['type'] = 'boolean'
+        items['noyes']['enumerators'] = {0: 'No', 1: 'Yes'}
+
+        items['number'] = dict()
+        items['number']['description'] = 'A numeric item.'
+        items['number']['type'] = 'numeric'
+        items['number']['units'] = 'meaningless units'
+
+        items['readonly'] = dict()
+        items['readonly']['description'] = 'A read-only numeric item.'
+        items['readonly']['type'] = 'numeric'
+        items['readonly']['units'] = 'meaningless units'
+        items['readonly']['initial'] = 13
+        items['readonly']['settable'] = False
+
+        items['string'] = dict()
+        items['string']['description'] = 'A string item.'
+        items['string']['type'] = 'string'
+
+        items['typeless'] = dict()
+        items['typeless']['description'] = 'A typeless item.'
+
+        return items
 
 
 # end of class Daemon
 
-
-
-def generate_catalog():
-
-    items = dict()
-
-    items['angle'] = dict()
-    items['angle']['description'] = 'An angular numeric item.'
-    items['angle']['type'] = 'numeric'
-    items['angle']['format'] = '%2d:%2.2d:%04.1f'
-    items['angle']['units'] = {'': 'radians', 'formatted': 'degrees'}
-    items['angle']['initial'] = 0.018049613347708025
-
-    items['boolean'] = dict()
-    items['boolean']['description'] = 'A boolean item without enumerators.'
-    items['boolean']['type'] = 'boolean'
-
-    items['enumerated'] = dict()
-    items['enumerated']['description'] = 'An enumerated item.'
-    items['enumerated']['type'] = 'enumerated'
-    items['enumerated']['enumerators'] = {0: 'Zero', 1: 'One', 4: 'Four'}
-
-    items['hourangle'] = dict()
-    items['hourangle']['description'] = 'An angular numeric item, in h:m:s.'
-    items['hourangle']['type'] = 'numeric'
-    items['hourangle']['format'] = '%2d:%2.2d:%04.1f'
-    items['hourangle']['units'] = {'': 'radians', 'formatted': 'hours'}
-    items['hourangle']['initial'] = 0.2707442002156204
-
-    items['mask'] = dict()
-    items['mask']['description'] = 'A mask item.'
-    items['mask']['type'] = 'mask'
-    items['mask']['enumerators'] = {'None': 'none set', 0: 'A', 1: 'B', 2: 'C'}
-
-    items['noyes'] = dict()
-    items['noyes']['description'] = 'A boolean item with enumerators.'
-    items['noyes']['type'] = 'boolean'
-    items['noyes']['enumerators'] = {0: 'No', 1: 'Yes'}
-
-    items['number'] = dict()
-    items['number']['description'] = 'A numeric item.'
-    items['number']['type'] = 'numeric'
-    items['number']['units'] = 'meaningless units'
-
-    items['readonly'] = dict()
-    items['readonly']['description'] = 'A read-only numeric item.'
-    items['readonly']['type'] = 'numeric'
-    items['readonly']['units'] = 'meaningless units'
-    items['readonly']['initial'] = 13
-    items['readonly']['settable'] = False
-
-    items['string'] = dict()
-    items['string']['description'] = 'A string item.'
-    items['string']['type'] = 'string'
-
-    items['typeless'] = dict()
-    items['typeless']['description'] = 'A typeless item.'
-
-    return items
 
 
 # vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
