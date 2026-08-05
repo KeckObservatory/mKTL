57032a4501884618b5e03c1152ebd07434402a03
Kyle Lanclos <klanclos@keck.hawaii.edu>
2026-08-04 14:48:46 -1000

    Docstrings and readability.

--- examples/heater/heater.py
+++ examples/heater/heater.py
@@ -13,6 +13,12 @@ import mktl
 class Daemon(mktl.Daemon):
 
     def parse_options(self, options):
+        """ Read in the configuration file; this daemon can't function without
+            some additional guidance on how it is supposed to run. This file
+            would define some metadata on how the controller is being used,
+            including system-specific names for temperature inputs and
+            heater outputs.
+        """
 
         configuration_file = options.appconfig
 
@@ -23,6 +29,9 @@ class Daemon(mktl.Daemon):
 
 
     def describe_items(self):
+        """ Generate and return the description of all items handled by
+            this daemon.
+        """
 
         items = dict()
 
@@ -30,7 +39,6 @@ class Daemon(mktl.Daemon):
         controller_items = self.describe_controller_items(controller_number)
 
         items.update(controller_items)
-
 
         try:
             inputs = self.heater_config.options('inputs')
@@ -44,43 +52,20 @@ class Daemon(mktl.Daemon):
 
         for input in inputs:
             prefix = main.config.get('inputs', input)
-
-            chi = prefix + 'chi'
-            items[chi] = dict()
-            items[chi]['description'] = 'Channel for this temperature input.'
-            items[chi]['settable'] = False
-
-            tmp = prefix + 'tmp'
-            items[tmp] = dict()
-            items[tmp]['description'] = 'Current temperature value.'
-            items[tmp]['type'] = 'numeric'
-            items[tmp]['units'] = 'deg C'
-            items[tmp]['settable'] = False
+            items.update(self.describe_input_items(prefix))
 
         for output in outputs:
-            cho = prefix + 'cho'
-            items[cho] = dict()
-            items[cho]['description'] = 'Channel for this heater output.'
-            items[cho]['settable'] = False
-
-            out = prefix + 'out'
-            items[out] = dict()
-            items[out]['description'] = 'Current heater output.'
-            items[out]['type'] = 'numeric'
-            items[out]['units'] = 'watts'
-            items[out]['settable'] = False
-
-            trg = prefix + 'trg'
-            items[trg] = dict()
-            items[trg]['description'] = 'Heater setpoint/target value.'
-            items[trg]['type'] = 'numeric'
-            items[trg]['units'] = 'deg C'
-
+            prefix = main.config.get('outputs', output)
+            items.update(self.describe_output_items(prefix))
 
         return items
 
 
     def describe_controller_items(self, controller):
+        """ Generation of the description of controller-wide items is broken
+            out here for readability. The *controller* argument is expected
+            to be a number.
+        """
 
         controller = str(controller)
         prefix = 'ctrl' + controller
@@ -106,6 +91,56 @@ class Daemon(mktl.Daemon):
         items[status]['description'] = 'Controller connection status.'
         items[status]['type'] = 'enumerated'
         items[status]['settable'] = False
+
+        return items
+
+
+    def describe_input_items(self, prefix):
+        """ Generation of the description of input-specific items is broken
+            out here for readability.
+        """
+
+        items = dict()
+
+        chi = prefix + 'chi'
+        items[chi] = dict()
+        items[chi]['description'] = 'Channel for this temperature input.'
+        items[chi]['settable'] = False
+
+        tmp = prefix + 'tmp'
+        items[tmp] = dict()
+        items[tmp]['description'] = 'Current temperature value.'
+        items[tmp]['type'] = 'numeric'
+        items[tmp]['units'] = 'deg C'
+        items[tmp]['settable'] = False
+
+        return items
+
+
+    def describe_output_items(self, prefix):
+        """ Generation of the description of output-specific items is broken
+            out here for readability.
+        """
+
+        items = dict()
+
+        cho = prefix + 'cho'
+        items[cho] = dict()
+        items[cho]['description'] = 'Channel for this heater output.'
+        items[cho]['settable'] = False
+
+        out = prefix + 'out'
+        items[out] = dict()
+        items[out]['description'] = 'Current heater output.'
+        items[out]['type'] = 'numeric'
+        items[out]['units'] = 'watts'
+        items[out]['settable'] = False
+
+        trg = prefix + 'trg'
+        items[trg] = dict()
+        items[trg]['description'] = 'Heater setpoint/target value.'
+        items[trg]['type'] = 'numeric'
+        items[trg]['units'] = 'deg C'
 
         return items
 
