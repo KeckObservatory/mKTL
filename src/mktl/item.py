1b4808e3c15fb8d3d7daa3af07e1196532db549b
Kyle Lanclos <klanclos@keck.hawaii.edu>
2026-08-19 14:42:59 -1000

    Break up the background queue into three queues.

--- src/mktl/item.py
+++ src/mktl/item.py
@@ -63,9 +63,13 @@ class Item:
 
         try:
             # Available in Python 3.7+.
-            self._backgrounded = queue.SimpleQueue()
+            self._get_queue = queue.SimpleQueue()
+            self._pub_queue = queue.SimpleQueue()
+            self._set_queue = queue.SimpleQueue()
         except AttributeError:
-            self._backgrounded = queue.Queue()
+            self._get_queue = queue.Queue()
+            self._pub_queue = queue.Queue()
+            self._set_queue = queue.Queue()
 
         self._updated = threading.Event()
 
@@ -123,7 +127,7 @@ class Item:
             ### for a single item are processed in order of receipt and not
             ### in parallel.
 
-            def wrap(request, queue=self._backgrounded, req_set=self.req_set):
+            def wrap(request, queue=self._set_queue, req_set=self.req_set):
                 """ The wrapper around self.req_set() allows the request to
                     be handled by a background worker thread, assigned by the
                     _Sequencer background thread.
@@ -560,7 +564,7 @@ class Item:
 
         # Hand off the incoming message for background processing.
 
-        pending = self._backgrounded
+        pending = self._pub_queue
         task = _Task(self._update, message)
         pending.put(task)
         _Sequencer.pending.put(pending)
@@ -627,7 +631,7 @@ class Item:
             message = protocol.message.Broadcast('PUB', key, payload)
 
             # One could bypass the normal broadcast handling internally
-            # within a daemon by putting the message in self._backgrounded
+            # within a daemon by putting the message in self._pub_queue
             # instead of relying on the full ZeroMQ-based broadcast handling.
             # This would be more efficient, but there is something to be said
             # for fully exercising the normal handling chain in identical
@@ -754,8 +758,8 @@ class Item:
                     item.rep.respond(request, payload, error)
 
             task = _Task(wrap, request)
-            self._backgrounded.put(task)
-            _Sequencer.pending.put(self._backgrounded)
+            self._get_queue.put(task)
+            _Sequencer.pending.put(self._get_queue)
             payload = None
         else:
             payload = self.to_payload()
@@ -1536,11 +1540,9 @@ class _Sequencer:
             try:
                 task.execute()
             except:
-                debug = traceback.format_exc()
-                message = 'mKTL background task error:'
                 logger = logging.getLogger(__name__)
-                logger.error(message)
-                logger.error(debug)
+                logger.error('mKTL background task error:')
+                logger.error(traceback.format_exc())
 
 
     def run(self):
