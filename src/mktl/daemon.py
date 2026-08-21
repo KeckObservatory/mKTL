2095b55d206ab67afc1131844b7fb201990dfcbb
Kyle Lanclos <klanclos@keck.hawaii.edu>
2026-08-20 15:58:42 -1000

    Allow running a daemon with an empty initial configuration.

--- src/mktl/daemon.py
+++ src/mktl/daemon.py
@@ -130,6 +130,13 @@ class Daemon:
         # consistency.
 
         block = self.catalog.authoritative_block
+
+        if block is None:
+            # Running with an empty configuration. This is not very useful,
+            # but is not explicitly forbidden, so make allowances for it here.
+            meta.authoritative(store, alias, dict())
+            block = self.catalog.authoritative_block
+
         meta.add_provenance(block, self.rep.hostname, self.rep.port, self.pub.port)
         self.catalog.update(block)
 
