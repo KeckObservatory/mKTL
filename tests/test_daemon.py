c4662173df106a139d12dc393da2a9dcd6b74c53
Kyle Lanclos <klanclos@keck.hawaii.edu>
2026-08-25 16:01:49 -1000

    Test item interactions.

--- tests/test_daemon.py
+++ tests/test_daemon.py
@@ -41,4 +41,67 @@ def test_subclass_item(run_mkregistryd):
     Daemon('unittest_daemon_subclass_item', 'unittest', override=True)
 
 
+def test_subclass_item_interactions(run_mkregistryd):
+
+    class Something(mktl.Item):
+
+        def perform_get(self):
+            return self.value + 1
+
+    class Payloader(mktl.Item):
+
+        def perform_get(self):
+            payload = mktl.protocol.message.Payload(value=self.value)
+            return payload
+
+    class Daemon(mktl.Daemon):
+
+        def describe_items(self):
+            items = dict()
+
+            items['payloader'] = dict()
+            items['payloader']['description'] = 'A test item'
+            items['payloader']['type'] = 'string'
+            items['payloader']['initial'] = 'testing'
+
+            items['something'] = dict()
+            items['something']['description'] = 'A test item'
+            items['something']['type'] = 'numeric'
+            items['something']['initial'] = 0
+
+            return items
+
+        def setup(self):
+            self.add_item(Something, 'something')
+            self.add_item(Payloader, 'payloader')
+
+    Daemon('unittest_daemon_subclass_item_interact', 'unittest', override=True)
+
+    something = mktl.get('unittest_daemon_subclass_item_interact', 'something')
+
+    something.set(40)
+    assert something == 40
+
+    something.get(refresh=True)
+    assert something == 41
+
+    something.get(refresh=True)
+    assert something == 42
+
+    something.value = 50
+    assert something.get() == 50
+
+    something.publish(66)
+    assert something.value == 66
+    assert something.get() == 66
+
+    payloader = mktl.get('unittest_daemon_subclass_item_interact', 'payloader')
+
+    assert payloader.value == 'testing'
+    payloader.value = 'testing elsewise'
+    assert payloader.value == 'testing elsewise'
+    assert payloader.get() == 'testing elsewise'
+
+
+
 # vim: set expandtab tabstop=8 softtabstop=4 shiftwidth=4 autoindent:
